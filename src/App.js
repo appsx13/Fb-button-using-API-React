fc1c7ae58a05238fe390f91d78b8f9ab62e5e12c
Apoorva <apps3660.com>
2020-08-19 19:58:48 +0530

    facebook login button using API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Facebook from './components/facebook';
+
 
 function App() {
   return (
@@ -8,18 +10,18 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Facebook Auth Example
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <Facebook />
+        </div>
+        <br/>
+        <p>
+          To get started, authenticate with facebook
+        </p>
       </header>
     </div>
+    
   );
 }
 
